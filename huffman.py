a6966e10c1a4de9459b3f57e02864da3547f4420
tarhengal <theo17430@gmail.com>
2022-05-30 00:34:30 +0200

    ajout de l'extraction du fichier .bin et du parcours de l'arbre

--- huffman.py
+++ huffman.py
@@ -10,15 +10,9 @@ def huffman_tree(freq):
         nodes.sort(key=lambda x: (x.freq,x.label))
         l_child = nodes[0]
         r_child = nodes[1]
-        new_node = Node(l_child.freq+r_child.freq,str(l_child.freq+r_child.freq),l_child,r_child)
+        new_node = Node(l_child.freq+r_child.freq,l_child.label+r_child.label,l_child,r_child)
         nodes.remove(l_child)
         nodes.remove(r_child)
         nodes.append(new_node)
-        print(nodes)
-    
-    return nodes
 
-freq = extract("exemple_freq.txt")
-huffman_tree(freq)
-#print(freq)
-#print(huffman_tree(freq))
+    return nodes
